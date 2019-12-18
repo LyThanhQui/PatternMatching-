b2d1e4cffb8fe62cbb70b426e50489bfc6375213
Qui <lquiitfuture@gmail.com>
2019-12-18 22:30:13 +0700

    change Program.cs file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace PatternMatching
 {
     //example 1
     //Pattern Matching in C# with “is” expression:
+    //Pattern Matching Before C# 7:
     public class Shape
     {
         public const float PI = 3.14f;
@@ -36,5 +37,42 @@ namespace PatternMatching
             Height = height;
         }
     }
+
+    class Program
+    {
+        static void Main()
+        {
+            Circle circle = new Circle(10);
+            DisplayArea(circle);
+            Rectangle rectangle = new Rectangle(10, 5);
+            DisplayArea(rectangle);
+            Triangle triangle = new Triangle(10, 5);
+            DisplayArea(triangle);
+            Console.ReadKey();
+        }
+        public static void DisplayArea(Shape shape)
+        {
+            if (shape is Circle)
+            {
+                Circle c = (Circle)shape;
+                Console.WriteLine("Area of Circle is : " + c.Radius * c.Radius * Shape.PI);
+            }
+            else if (shape is Rectangle)
+            {
+                Rectangle r = (Rectangle)shape;
+                Console.WriteLine("Area of Rectangle is : " + r.Length * r.Height);
+            }
+            else if (shape is Triangle)
+            {
+                Triangle t = (Triangle)shape;
+                Console.WriteLine("Area of Triangle is : " + 0.5 * t.Base * t.Height);
+            }
+            else
+            {
+                throw new ArgumentException(message: "Invalid Shape", paramName: nameof(shape));
+            }
+        }
+    }
+
 
 }
